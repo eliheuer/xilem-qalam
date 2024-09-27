b6f7a2c894f8e199b391593ab64a44b8821cd5f6
Eli Heuer <elih@protonmail.com>
2024-09-26 23:23:06 -0700

    add xilem hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use masonry;
+use masonry::{dpi, widget};
+use winit::window;
+use xilem;
+use xilem::view;
+
+struct HelloWorld;
+
 fn main() {
-    println!("Hello, world!");
+    let app = xilem::Xilem::new(HelloWorld, app_logic);
+    let window_attributes = window::Window::default_attributes()
+        .with_title("Hello World")
+        .with_inner_size(dpi::LogicalSize::new(300, 200));
+    app.run_windowed_in(xilem::EventLoop::with_user_event(), window_attributes)
+        .unwrap();
 }
+    
+fn app_logic(_: &mut HelloWorld) -> impl xilem::WidgetView<HelloWorld> {
+    view::label("Hello, World!")
+}
+    
