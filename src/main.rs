eea9267922a3bfbd6a27c4c8aeea552663a36577
Eli Heuer <elih@protonmail.com>
2024-10-01 17:34:04 -0700

    start basic xilem port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+//! A font editor.
+
+use runebender_lib::data::{AppState, Workspace};
+
 use masonry::dpi;
 use winit::window;
 use xilem::{self, view};
@@ -16,3 +20,27 @@ fn main() {
 fn app_logic(_: &mut HelloWorld) -> impl xilem::WidgetView<HelloWorld> {
     view::label("Hello, World!")
 }
+
+/// If there was an argument passed at the command line, try to open it as a .ufo
+/// file, otherwise return blank state.
+fn get_initial_state() -> AppState {
+    let (font_file, path) = if let Some(arg) = std::env::args().nth(1) {
+        match norad::Ufo::load(&arg) {
+            Ok(ufo) => (ufo, Some(std::path::PathBuf::from(arg))),
+            Err(e) => {
+                eprintln!(
+                    "Failed to load first arg '{}' as ufo file.\nError:'{}'",
+                    arg, e
+                );
+                std::process::exit(1);
+            }
+        }
+    } else {
+        (runebender_lib::create_blank_font(), None)
+    };
+
+    let mut workspace = Workspace::default();
+    workspace.set_file(font_file, path);
+    AppState { workspace }
+}
+
